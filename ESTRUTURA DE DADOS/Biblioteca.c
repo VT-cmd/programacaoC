32a225f19e7c96b8a2a03428f7dcf7c1c6298889
VT-cmd <vihumoliveira@gmail.com>
2025-10-28 04:55:53 +0000

    atualização estrutura de dados

--- ESTRUTURA DE DADOS/Biblioteca.c
+++ ESTRUTURA DE DADOS/Biblioteca.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <string.h> // Para strcspn()
 
-
+// ---Constantes Globais---
 #define MAX_LIVROS 50
 #define TAM_STRING 100
 
+// --- Estrutura (Struct) ---
 struct Livro{
     char nome[TAM_STRING];
-    char autora[TAM_STRING];
+    char autor[TAM_STRING];
     char editora[TAM_STRING];
     int edicao;
 };
 
+
+// ---LIMPA BUFFER ---
 void limpabuffentrada(){
-int c;
-while (getchar() != '\n' && c != EOF);                  //LIMPA BUFFER
+    int c;
+    while (getchar() != '\n' && c != EOF);
 }
 
+// --- Função principal (main) --- 
 int main(){
     struct Livro biblioteca[MAX_LIVROS];
     int totalLivros= 0;
     int opcao;
 
+// --- Laço principal ---
 do{
 
+     // --- Menu de opções ---
     printf("===============================\n");
     printf("      Biblioteca - Parte 1     \n");
     printf("===============================\n");
@@ -34,15 +40,52 @@ do{
     printf("-------------------------------\n");
     printf("Escolha uma opção:");
 
+    // --- lê a opção do usuário ---
     scanf("d%", &opcao);
     limpabuffentrada();
 
 
+
+    // --- processamento da opção ---
+    switch (opcao) {
+        case 1:
+            printf("--- Cadastro de Novo Livro ---\n\n");
+
+            if (totalLivros < MAX_LIVROS) {
+                printf("Digite o nome do livro: ");
+                fgets(biblioteca[totalLivros].nome, TAM_STRING, stdin);
+
+                printf("Digite o nome do autor: ");
+                fgets(biblioteca[totalLivros].autor, TAM_STRING, stdin);
+
+                printf("Digite a editora: ");
+                fgets(biblioteca[totalLivros].editora, TAM_STRING, stdin);
+
+
+                biblioteca[totalLivros].nome[strcspn(biblioteca[totalLivros].nome, "\n")] = '\0';
+                biblioteca[totalLivros].autor[strcspn(biblioteca[totalLivros].autor, "\n")] = '\0';
+                biblioteca[totalLivros].editora[strcspn(biblioteca[totalLivros].editora, "\n")] = '\0';
+
+                printf("Digite a edição: ");
+                scanf("%d", &biblioteca[totalLivros].edicao);
+                limpabuffentrada();
+
+                totalLivros++;
+
+
+            }
+
+    }
+    
+
+
+
+
+
+
+
+
 }
-
-
-
-
 
 
 }
